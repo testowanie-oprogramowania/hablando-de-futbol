54f00805ffcdf3e766b56fb00c5aa4d15b6b96c7
Sebastian Pietrykowski <s_pietrykowski@outlook.com>
2023-11-25 01:46:51 +0100

    Added icon in navbar

--- football-frontend/src/app/app.component.ts
+++ football-frontend/src/app/app.component.ts
@@ -1,7 +1,7 @@
 import { Component, isDevMode, OnInit } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { RouterOutlet } from '@angular/router';
-import {NavbarComponent} from "./navbar/navbar.component";
+import { NavbarComponent } from './navbar/navbar.component';
 
 @Component({
   selector: 'app-root',
