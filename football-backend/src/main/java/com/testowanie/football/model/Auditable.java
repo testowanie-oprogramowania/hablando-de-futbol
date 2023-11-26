2d30e774bbd83038a8d9bdc6831c5aa362b93bae
adamsm2 <95346590+adamsm2@users.noreply.github.com>
2023-11-26 20:51:49 +0100

    Added auditing (#14)

---
+++ football-backend/src/main/java/com/testowanie/football/model/Auditable.java
@@ -0,0 +1,29 @@
+package com.testowanie.football.model;
+
+import jakarta.persistence.Column;
+import jakarta.persistence.EntityListeners;
+import jakarta.persistence.MappedSuperclass;
+import lombok.*;
+import lombok.experimental.SuperBuilder;
+import org.springframework.data.annotation.CreatedDate;
+import org.springframework.data.annotation.LastModifiedDate;
+import org.springframework.data.jpa.domain.support.AuditingEntityListener;
+
+import java.time.Instant;
+
+@Setter
+@Getter
+@SuperBuilder
+@NoArgsConstructor(access = AccessLevel.PROTECTED)
+@AllArgsConstructor
+@MappedSuperclass
+@EntityListeners(AuditingEntityListener.class)
+public abstract class Auditable {
+
+    @CreatedDate
+    @Column(updatable = false)
+    private Instant createdDate;
+
+    @LastModifiedDate
+    private Instant lastModifiedDate;
+}
---
+++ football-backend/src/main/java/com/testowanie/football/repository/EditorRepository.java
@@ -0,0 +1,7 @@
+package com.testowanie.football.repository;
+
+import com.testowanie.football.model.Editor;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface EditorRepository extends JpaRepository<Editor, Long> {
+}
