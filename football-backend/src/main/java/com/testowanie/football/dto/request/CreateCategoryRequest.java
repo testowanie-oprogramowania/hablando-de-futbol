4a156b22bc14b1001d4150b7e9b97c73b0b72c71
Wiktor Nasierowski <wiktor.nasierowski@gmail.com>
2023-11-27 23:27:40 +0100

    Bob the builder laid off

--- football-backend/src/main/java/com/testowanie/football/dto/request/CreateCategoryRequest.java
+++ football-backend/src/main/java/com/testowanie/football/dto/request/CreateCategoryRequest.java
@@ -2,11 +2,7 @@ package com.testowanie.football.dto.request;
 
 import jakarta.validation.constraints.NotBlank;
 import jakarta.validation.constraints.Size;
-import lombok.Builder;
-import lombok.extern.jackson.Jacksonized;
 
-@Builder
-@Jacksonized
 public record CreateCategoryRequest(
         @NotBlank
         @Size(min = 3, max = 200)
