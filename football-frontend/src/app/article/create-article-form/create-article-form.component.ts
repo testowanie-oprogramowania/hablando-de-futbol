a62f5e65137dbb1f730c5e3ade514f3c036f50da
Sebastian Pietrykowski <s_pietrykowski@outlook.com>
2023-11-28 00:17:30 +0100

    Added create article form, secttionalized forms

--- football-frontend/src/app/article/create-article-form/create-article-form.component.ts
+++ football-frontend/src/app/article/create-article-form/create-article-form.component.ts
@@ -1,13 +1,66 @@
 import { Component } from '@angular/core';
 import { CommonModule } from '@angular/common';
+import {
+    FormBuilder,
+    FormGroup,
+    FormsModule,
+    ReactiveFormsModule,
+} from '@angular/forms';
+import { Editor } from '../../models/editor';
+import { MatFormFieldModule } from '@angular/material/form-field';
+import { MatInputModule } from '@angular/material/input';
+import { MatDatepickerModule } from '@angular/material/datepicker';
+import { MatSelectModule } from '@angular/material/select';
+import { Observable, of } from 'rxjs';
+import { MatButtonModule } from '@angular/material/button';
+import { MatNativeDateModule } from '@angular/material/core';
+import { Category } from '../../models/category';
+import { MatCardModule } from '@angular/material/card';
+import { TextInputFieldComponent } from '../../shared/text-input-field/text-input-field.component';
+import { TextAreaFieldComponent } from '../../shared/text-area-field/text-area-field.component';
+import { DatePickerFieldComponent } from '../../shared/date-picker-field/date-picker-field.component';
+import { SelectFieldComponent } from '../../shared/select-field/select-field.component';
 
 @Component({
-  selector: 'app-create-article-form',
-  standalone: true,
-  imports: [CommonModule],
-  templateUrl: './create-article-form.component.html',
-  styleUrl: './create-article-form.component.scss'
+    selector: 'app-create-article-form',
+    standalone: true,
+    imports: [
+        CommonModule,
+        MatFormFieldModule,
+        ReactiveFormsModule,
+        MatInputModule,
+        MatDatepickerModule,
+        MatSelectModule,
+        MatButtonModule,
+        FormsModule,
+        MatNativeDateModule,
+        MatFormFieldModule,
+        MatCardModule,
+        TextInputFieldComponent,
+        TextAreaFieldComponent,
+        DatePickerFieldComponent,
+        SelectFieldComponent,
+    ],
+    templateUrl: './create-article-form.component.html',
+    styleUrl: './create-article-form.component.scss',
 })
 export class CreateArticleFormComponent {
+    articleForm = this.formBuilder.group({
+        title: [''],
+        editor: [''],
+        publicationDate: [''],
+        content: [''],
+        photoUrl: [''],
+        category: [''],
+    });
 
+    categories$: Observable<Category[]> = of();
+    categoryFormToShow = (category: Category) => category.name;
+
+    editors$: Observable<Editor[]> = of();
+    editorFormToShow = (editor: Editor) => editor.name;
+
+    constructor(private readonly formBuilder: FormBuilder) {}
+
+    submitForm() {}
 }
---
+++ football-frontend/src/app/shared/text-area-field/text-area-field.component.ts
@@ -0,0 +1,22 @@
+import {Component, Input} from '@angular/core';
+import { CommonModule } from '@angular/common';
+import {FormControl, ReactiveFormsModule} from "@angular/forms";
+import {MatFormFieldModule} from "@angular/material/form-field";
+import {MatInputModule} from "@angular/material/input";
+
+@Component({
+    selector: 'app-text-area-field',
+    standalone: true,
+    imports: [
+        CommonModule,
+        MatFormFieldModule,
+        MatInputModule,
+        ReactiveFormsModule,
+    ],
+    templateUrl: './text-area-field.component.html',
+    styleUrl: './text-area-field.component.scss',
+})
+export class TextAreaFieldComponent {
+    @Input() label: string = '';
+    @Input() control: FormControl = new FormControl();
+}
