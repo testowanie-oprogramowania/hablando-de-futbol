daa90e3a30839d5f71c60ab11cc2499b0de0de5a
Sebastian Pietrykowski <s_pietrykowski@outlook.com>
2023-11-29 12:54:12 +0100

    Updated common component for showing page

--- football-frontend/src/app/shared/show-page/show-page.component.ts
+++ football-frontend/src/app/shared/show-page/show-page.component.ts
@@ -2,7 +2,7 @@ import {Component, Input} from '@angular/core';
 import { CommonModule } from '@angular/common';
 import {MatButtonModule} from "@angular/material/button";
 import {MatCardModule} from "@angular/material/card";
-import {FormGroup, ReactiveFormsModule} from "@angular/forms";
+import {FormGroup, FormsModule, ReactiveFormsModule} from "@angular/forms";
 import {SelectFieldComponent} from "../select-field/select-field.component";
 import {TextAreaFieldComponent} from "../text-area-field/text-area-field.component";
 import {TextInputFieldComponent} from "../text-input-field/text-input-field.component";
@@ -18,11 +18,12 @@ import {TextInputFieldComponent} from "../text-input-field/text-input-field.comp
         SelectFieldComponent,
         TextAreaFieldComponent,
         TextInputFieldComponent,
+        FormsModule,
     ],
     templateUrl: './show-page.component.html',
     styleUrl: './show-page.component.scss',
 })
 export class ShowPageComponent {
-    @Input() formGroupA: FormGroup = new FormGroup({})
+    @Input() isForm: boolean = false;
     @Input() onSubmitForm: () => void = () => {};
 }
